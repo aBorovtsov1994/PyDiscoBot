f7306824f2baf3c7b4c2d96874a286c1896027d8
Aleks Borovtsov <aBorovtsov1994@users.noreply.github.com>
2020-10-07 04:08:15 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+#using .env files as the environmental variables for the bot and server tokens to be used in the bot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
