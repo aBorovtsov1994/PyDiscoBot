4127857a1ad41e84f703c3db71b9cb8bc15eefd0
aBorovtsov1994 <Konfeta04@gmail.com>
2020-10-23 16:42:17 -0400

    Added packages for environmental variables to work with the bot. Added bot token to environmental variables file.

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
-#bot.py
+# bot.py
 import os
 
 import discord
 from dotenv import load_dotenv
-#using .env files as the environmental variables for the bot and server tokens to be used in the bot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
+
