81c0d6aa48312be7261b937ab33548d7cbb11d5d
aBorovtsov1994 <Konfeta04@gmail.com>
2020-12-08 15:32:03 -0500

    Added another print statement to help show how to use the git bash!

--- bot.py
+++ bot.py
@@ -24,5 +24,6 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
     print('Hello guys! Welcome to the server!')
+    print('Made another change!')
 
 client.run(TOKEN)
