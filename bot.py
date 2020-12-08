9f5e5ff5a5b76a04e005c26759dd97bb074f4de2
aBorovtsov1994 <Konfeta04@gmail.com>
2020-12-08 15:22:40 -0500

    This is an update to my example branch!

--- bot.py
+++ bot.py
@@ -23,6 +23,6 @@ async def on_ready():
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
-    print('Hello guys!')
+    print('Hello guys! Welcome to the server!')
 
 client.run(TOKEN)
